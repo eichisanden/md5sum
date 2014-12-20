b53c2b68abdd520e2264cf790d3dbb6fa2f1c2c9
Eiichi Mita <parkizz@gmail.com>
2014-12-20 21:36:02 +0900

    Change from jQuery GET function to XMLHttpRequest

--- js/background.js
+++ js/background.js
@@ -3,16 +3,19 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 });
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
-	$.get(
-		info.linkUrl,
-		{},
-		function(data) {
-			//var result = MD5_hexhash(data);
-			var result = MD5_hash(data);
-			alert(result);
-			return false;
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', info.linkUrl, true);
+	xhr.responseType = "arraybuffer";
+	xhr.onload = function() {
+		var bytes = new Uint8Array(this.response);
+		var binaryData = "";
+		for (var i = 0, len = bytes.byteLength; i < len; i++) {
+			binaryData += String.fromCharCode(bytes[i]);
 		}
-	);
+		var result = MD5_hexhash(binaryData);
+		alert(result);
+	};
+	xhr.send();
 });
 
 chrome.contextMenus.create({
